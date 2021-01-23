0ab192c53b5c818a0a897519d66e123a460da5e9
Yu Ohno <kantillo0683@gmail.com>
2021-01-23 15:24:32 +0900

    apply:fontplus

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
             })(document);`,
           }}>
           </script>
-
+          <script type="text/javascript" src="//webfont.fontplus.jp/accessor/script/fontplus.js?QMuq206N5yA%3D&box=pSd-Pe6CAmY%3D&delay=2&timeout=3&pm=1&aa=1&ab=2" charset="utf-8"></script>
           <script type="text/javascript" dangerouslySetInnerHTML={{
             __html: `(function(w,s){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src="https://cdn.pagesense.io/js/desksamplelab/f4bc4d4426ae46e1b835c8ddfef3fa96.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(e,x);})(window,"script");`,
           }}></script>
